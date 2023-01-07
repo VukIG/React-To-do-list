f52574981c9371e271761c0917b4186c2ef61bfb
VukIG <vuk.ignjatovic05@gmail.com>
2023-01-06 15:09:42 -0900

    prvi comit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='App'>
+      <input className='' placeholder='Enter task name:' type="text" />
+    </div>
+  )
+}
+
+export default App
