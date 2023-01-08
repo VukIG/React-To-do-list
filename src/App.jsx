00ca3cdc76b04d9bdb721bbf80d1d7165532b2dd
VukIG <vuk.ignjatovic05@gmail.com>
2023-01-07 16:35:05 -0900

    chatgpt odradio pola posla kakav smeker

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,39 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import React from 'react'
+import { v4 as uuidv4 } from 'uuid';
 
 function App() {
-  const [tasks,setTasks] = useState('');
+  const [tasks,setTasks] = useState([]);
   function clearTasks() {
-    setTasks('');
+    setTasks([]);
   }
 
-  useEffect((
-    tasks.map( task => <li key={uui4()}>{task}</li>)
-  ),[])
+  function removeTask(key) {
+    const newTasks = tasks.filter( task => task.key != key )
+    setTasks(newTasks);
+  }
+  
+  function addTask(task) {
+    const newTask = {
+      key:uuidv4(),
+      task:task,
+    };
+    setTasks([...tasks,newTask])
+  }
 
   return (
     <div >
       <div className='bg-black absolute top-[50%] left-[50%] flex justify-center translate-x-[-50%] translate-y-[-50%]'>
         <input className='w-[400px] align-middle 
         m-5 p-2 rounded' placeholder='Enter task name:' type="text" />
-        <button className='m-5 bg-red-700 text-white p-5' onClick={(e)=>{setTasks(...tasks, e.target.value)}}>Add tasks</button>
-        <button className='m-5 bg-green-700 text-white p-5' onClick={clearTasks}>Clear all tasks</button>
+        <button className='m-5 bg-green-700 text-white p-5' onClick={(e)=>{addTask(e.target.value)}}>Add tasks</button>
+        <button className='m-5 bg-red-700 text-white p-5' onClick={clearTasks}>Clear all tasks</button>
       </div>
-      <div className="">{allTasks}</div>
+      <div className="">
+        <ul>
+          {tasks.map( task => <li key={task.key} onClick={ () => removeTask(task.key) }> {task.task} </li>)}
+        </ul>
+      </div>
     </div>
   )
 }
